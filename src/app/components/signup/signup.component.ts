facb1c4f1698cd06fc81346343bee921cbcfabea
Edgar Estrada <estradacalvilloedgar@gmail.com>
2021-06-30 18:59:50 -0500

    Login Validations

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -12,10 +12,9 @@ export class SignupComponent implements OnInit {
   user = {
     firstName:'',
     lastName:'',
-    username:'',
     email:'',
     password:'',
-    city:''
+    userType:''
   }
   
 
@@ -23,17 +22,54 @@ export class SignupComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  esEmailValido(email: string):boolean {
+    let mailValido = false;
+      'use strict';
+
+      let EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+      if (email.match(EMAIL_REGEX)){
+        mailValido = true;
+      }
+    return mailValido;
+  }
   signUp(){
-    this.authService.signUp(this.user)
-      .subscribe(
-        res=>{
-          console.log(res)
-          this.router.navigate(['signin']);
-        },
-        err=>{
-          console.log(err)
-        }
-      )
+    if(this.user.firstName == "" || this.user.lastName == "" || this.user.email == "" || this.user.password == "" || this.esEmailValido(this.user.email) == false){
+      console.log("Error")
+    }else{
+      let userType = this.user.email.slice(0, 6);
+      console.log(userType);
+      //1 Admin - 2 Student
+      if(!isNaN(Number(userType))){
+        console.log("Student")
+        this.user.userType='2';
+        console.log("User Student: " + JSON.stringify(this.user))
+        // this.authService.signUp(this.user)
+        // .subscribe(
+        //   res=>{
+        //     console.log(res)
+        //     this.router.navigate(['signin']);
+        //   },
+        //   err=>{
+        //     console.log(err)
+        //   }
+        // )
+      }else{
+        console.log("Admin")
+        this.user.userType='1';
+        console.log("User Admin: " + JSON.stringify(this.user))
+        // this.authService.signUp(this.user)
+        // .subscribe(
+        //   res=>{
+        //     console.log(res)
+        //     this.router.navigate(['signin']);
+        //   },
+        //   err=>{
+        //     console.log(err)
+        //   }
+        // )
+      }
+    }
   }
 
 }
