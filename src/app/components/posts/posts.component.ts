49f46f8d6246fee2b903c081613d13ae9640df35
Edgar Estrada <estradacalvilloedgar@gmail.com>
2021-08-09 23:40:52 -0500

    Channel View

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -123,7 +123,6 @@ export class PostsComponent implements OnInit {
           Swal.fire({
             icon: 'error',
             title: 'Oops...',
-            text: 'Error!'
           })
         }
       )
--- src/app/components/view-channel/view-channel.component.ts
+++ src/app/components/view-channel/view-channel.component.ts
@@ -10,16 +10,31 @@ import { GeneralService } from '../../service/general-service.service'
 export class ViewChannelComponent implements OnInit {
   ListImages: any [];
   usertype : number
+  channel: boolean
+  arrPosts: any[]
+  channelID: any
+
+  nameChannel: any
+  descriptionChanne: any
+  postChannel: any
+
   post= {
-    channel_id: 4,
+    channel_id: 0,
     title: '',
     image_url: '',
     content: ''
   }
 
+  addChannel = {
+    user_id:this.generalService.user_id,
+    name:'',
+    description:''
+  }
+
   constructor(private generalService : GeneralService, private router : Router) { }
 
   ngOnInit(): void {
+    this.getChannel();
   }
 
   getURL(url:string){
@@ -44,6 +59,8 @@ export class ViewChannelComponent implements OnInit {
   }
 
   newPost(){
+    console.log("****************************************************")
+    this.post.channel_id = this.generalService.user_channel
     console.log(this.post)
     this.generalService.newPost(this.post)
       .subscribe(
@@ -55,5 +72,43 @@ export class ViewChannelComponent implements OnInit {
           console.log(err)
         }
       )
+  }
+
+  getChannel(){
+    this.generalService.getUserChannel(this.generalService.user_id)
+    .subscribe(res=>{
+      console.log(res)
+      if(res.Message=="No channels found"){
+        this.channel = false;
+        console.log("No hay")
+      }else{
+        console.log("*************************")
+        console.log(res)
+        this.channel = true;
+        this.generalService.user_channel = res[0].channel_id
+        this.postChannel = res[0].postquantity
+        this.descriptionChanne = res[0].description
+        this.nameChannel = res[0].name
+        this.getPostChannel(res[0].channel_id)
+      }
+    })
+  }
+
+  newChannel(){
+    this.generalService.newChannel(this.addChannel)
+    .subscribe(res=>{
+      console.log(res)
+    })
+  }
+
+  getPostChannel(id){
+    this.generalService.getPostChannel(id)
+    .subscribe(res=>{
+      console.log("*****  Ayuda ******")
+      let data = JSON.stringify(res);
+      let dataJson = JSON.parse(data);  
+      this.arrPosts = dataJson
+      console.log(this.arrPosts)
+    })
   }
 }
