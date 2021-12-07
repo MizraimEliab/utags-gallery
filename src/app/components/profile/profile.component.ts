866a18c6b4d8f316f6eba0ab1031d0c247878dcf
Mizraim Eliab <mizraimeliab168@gmail.com>
2021-12-06 20:55:18 -0600

    mic_2

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -64,7 +64,7 @@ export class ProfileComponent implements OnInit {
     var options = {
       mimeType: "audio/wav",
       numberOfAudioChannels: 1,
-      sampleRate: 16000,
+      sampleRate: 8000,
     };
     //Start Actuall Recording
     var StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
